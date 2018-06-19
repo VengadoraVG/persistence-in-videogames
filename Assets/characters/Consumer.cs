da6f9c82ed1f2832be9b8a41474132f7adcc6f0b
VG <vengadoravg@gmail.com>
2018-06-19 09:29:56 -0400

    remember the amount of fruits eaten by the player (THE SOLUTION)

--- Assets/characters/Consumer.cs
+++ Assets/characters/Consumer.cs
@@ -5,7 +5,20 @@ using System.Collections.Generic;
 public class Consumer : MonoBehaviour {
     public int consumed = 0;
 
+    void Start () {
+        if (PlayerPrefs.HasKey("fruits")) {
+            consumed = PlayerPrefs.GetInt("fruits");
+        } else {
+            consumed = 0;
+        }
+    }
+
+    public void Remember () {
+        PlayerPrefs.SetInt("fruits", consumed);
+    }
+
     void OnTriggerEnter (Collider c) {
         consumed++;
+        Remember();
     }
 }
