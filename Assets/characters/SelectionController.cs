294a5811625d9c92b0e04847f45268ad48edbf7c
VG <vengadoravg@gmail.com>
2018-06-19 01:40:15 -0400

    principles of character selection (THE SOLUTION)

---
+++ Assets/characters/SelectionController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SelectionController : MonoBehaviour {
+    public List<GameObject> availableCharacters;
+
+    private int _current = 0;
+
+    void Start () {
+        _Select(_current);
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            Previous();
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            Next();
+        }
+    }
+
+    private void _SetActive (GameObject thing, bool value) {
+        thing.GetComponent<Animator>().SetBool("running", value);
+        thing.transform.Find("arrow").gameObject.SetActive(value);
+    }
+
+    private void _Select (int index) {
+        _SetActive(availableCharacters[_current], false);
+        _current = index;
+        _SetActive(availableCharacters[_current], true);
+    }
+
+    public void Next () {
+        _Select((_current + 1) % availableCharacters.Count);
+    }
+
+    public void Previous () {
+        _Select(_current - 1 < 0? availableCharacters.Count-1: _current - 1);
+    }
+}
